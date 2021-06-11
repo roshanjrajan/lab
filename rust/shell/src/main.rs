cac6549e7c39cd6f0b008c2c19a76341f742b60a
roshanjrajan <roshanjrajan@gmail.com>
2021-06-11 00:12:11 -0500

    Creating basic loop for shell

--- rust/shell/src/main.rs
+++ rust/shell/src/main.rs
@@ -1,3 +1,23 @@
+use std::io;
+use std::io::Write;
+
+fn shell_loop()
+{
+    loop {
+        print!("> ");
+        io::stdout().flush().expect("Flushing errors");
+
+        // Read Input Arguments
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("Failed to read line");
+
+        let command_split = input.split_whitespace();
+        let command: Vec<&str> =  command_split.collect();
+
+        println!("{}", command[0]);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    shell_loop();
 }
