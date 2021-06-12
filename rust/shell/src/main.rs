021aef84b3043c3f644a31ef92bd6a643f7e8610
roshanjrajan <roshanjrajan@gmail.com>
2021-06-11 23:18:47 -0500

    Adding execution of input to shell

--- rust/shell/src/main.rs
+++ rust/shell/src/main.rs
@@ -1,5 +1,13 @@
+/*
+* Basic Shell
+*/
+use nix::sys::wait::wait;
+use nix::unistd::ForkResult::{Child, Parent};
+use nix::unistd::{fork, getpid, getppid};
 use std::io;
 use std::io::Write;
+use std::process::Command;
+
 
 fn shell_loop()
 {
@@ -11,10 +19,39 @@ fn shell_loop()
         let mut input = String::new();
         io::stdin().read_line(&mut input).expect("Failed to read line");
 
-        let command_split = input.split_whitespace();
-        let command: Vec<&str> =  command_split.collect();
+        let command = input.split_whitespace();
+        let tokens: Vec<&str> =  command.collect();
 
-        println!("{}", command[0]);
+        if tokens[0] == "quit" {break;}
+
+        println!("Executing command {}", tokens[0]);
+
+        Command::new(tokens[0])
+            .args(&tokens[1..])
+            .spawn()
+            .expect(&format!("{} command failed to start", tokens[0]));
+
+        io::stdout().flush().expect("Flushing errors");
+
+        /*
+        let pid = fork();
+
+        match pid.expect("Fork Failed: Unable to create child process!") {
+            Child => println!(
+                "Hello from child process with pid: {} and parent pid:{}",
+                getpid(),
+                getppid()
+            ),
+            Parent { child } => {
+                wait().expect("Wait Failed");
+                println!(
+                    "Hello from parent process with pid: {} and child pid:{}",
+                    getpid(),
+                    child
+                );
+            }
+        }
+        */
     }
 }
 
