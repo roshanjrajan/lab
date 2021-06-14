97d28dfaafb03a5e897d79363e0742539b24cb10
roshanjrajan <roshanjrajan@gmail.com>
2021-06-13 22:27:02 -0500

    Creating new solution and moving old solution to basic_elevator

--- javascript/elevator_game/elevator.js
+++ javascript/elevator_game/elevator.js
@@ -1,86 +1,63 @@
-// https://play.elevatorsaga.com/#challenge=4
 {
     init: function(elevators, floors) {
-        const weight = 0.4;
-        const maxFloor = floors.length-1;
+        const UP   = 0;
+        const DOWN = 1;
+        const PASS = 0;
+        const STOP = 1;
+        const TURN = 2;
+        const NUMFLOORS = Math.max(...floors.map(floor => floor.floorNum()));
 
-        let distance = function( elevator, floorNum) {
+        let distance = function(elevator, floorNum) {
             return Math.abs( elevator.currentFloor() - floorNum);
         }
 
-        let findIdle = function( floorNum) {
+        let findIdle = function(floorNum) {
             return elevators.filter( (elevator) => (elevator.destinationQueue.length == 0))
                 .sort( (a,b) => (distance(a, floorNum) - distance(b, floorNum)) );
         };
 
-        floors.forEach(function (floor) {
-            floor.on("up_button_pressed down_button_pressed", function () {
-                // find an idle elevator if possible
-                let choice = findIdle( floor.floorNum());
-                if (choice.length) {
-                    choice[0].goToFloor( floor.floorNum());
-                }
-            });
+
+        let setWaiting = function(floorNum, direction) {
+
+        };
+
+        floors.forEach(floor => {
+            floor.on('up_button_pressed',   () => setWaiting(floor.floorNum(), UP));
+            floor.on('down_button_pressed', () => setWaiting(floor.floorNum(), DOWN));
         });
 
-        elevators.forEach(function (elevator){
+        let handleApproach = function(elevator, floor) {
 
-            // Whenever the elevator is idle (has no more queued destinations) ...
-            elevator.on("idle", function() {
-                // let's go to all the floors (or did we forget one?)
+        };
 
-                let demand = floors.filter((floor) => (floor.buttonStates.up || floor.buttonStates.down));
 
-                // choose the first one
-                if (demand.length) {
-                    target = demand[0].floorNum();
-                } else {
-                    target = 0;
-                }
+        let handleStop = function(elevator, floor) {
+            if(elevator.currentFloor() == NUMFLOORS) {
+                elevator.direction = DOWN;
+                elevator.goingUpIndicator(false);
+                elevator.goingDownIndicator(true);
+            }
+            else if(elevator.currentFloor() == 0) {
+                elevator.direction = UP;
+                elevator.goingUpIndicator(true);
+                elevator.goingDownIndicator(false);
+            }
 
-                elevator.goToFloor(target);
-            });
+            // no passengers
+            if(elevator.loadFactor() == 0) {
 
-            elevator.on("floor_button_pressed", function(floorNum) {
-                let target = floorNum;
-                elevator.goToFloor(target);
-            });
+            }
+        };
 
-            elevator.on("passing_floor", function (floorNum, direction) {
-                let floor = floors[floorNum];
-                let pressed = elevator.getPressedFloors();
-                let stop = floor.buttonStates[direction] && elevator.loadFactor() < weight;
-                // if we're going in the same direction as the button, we can stop
-                if (stop || (pressed.indexOf( floorNum) >= 0)) {
-                    // remove this floor from destinations
-                    elevator.destinationQueue = elevator.destinationQueue.filter( (d) => (d !== floorNum));
-                    // no need to checkDestinationQueue as done in here...
-                    elevator.goToFloor(floorNum, true);
-                }
+        elevators.forEach(elevator =>
+            {
 
-            });
+            elevator.stops = new Set()
 
-            elevator.on("stopped_at_floor", function (floorNum) {
-                // do something here
-                // control up and down indicators
-                // TODO: control up down indicators better
-                switch (floorNum) {
-                    case 0:
-                        up = true;
-                        down = false;
-                        break;
-                    case maxFloor:
-                        up = false;
-                        down = true;
-                        break;
-                    default:
-                        up = true;
-                        down = true;
-                        break;
-                }
-                elevator.goingUpIndicator(up);
-                elevator.goingDownIndicator(down)
-            });
+            elevator.on('floor_button_pressed', floor => elevator.stops.add(floor));
+            elevator.on('passing_floor', floor => handleApproach(elevator, floor));
+            elevator.on('stopped_at_floor', floor => handleStop(elevator, floor));
+
         });
     },
         update: function(dt, elevators, floors) {
